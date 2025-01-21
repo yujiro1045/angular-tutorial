eaceeac3c79daeed486f38468bb7a7a795ae402b
Juan Medina <juanmedi730@gmail.com>
2025-01-21 09:57:43 -0500

    se separo la logica del listado y el agg personajes  en componentes

--- src/app/dbz/components/character/add-character.component.ts
+++ src/app/dbz/components/character/add-character.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'add-character',
+  selector: 'dbz-add-character',
   templateUrl: './add-character.component.html',
   standalone: false,
 })
