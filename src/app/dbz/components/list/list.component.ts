eb81e389042ddde24be4c867efe7f685d9068c35
Juan Medina <juanmedi730@gmail.com>
2025-01-22 21:31:27 -0500

    actualizacion de lista de personajes con ngFor

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   standalone: false,
 })
-export class ListComponent {}
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 15,
+    },
+  ];
+}
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
   standalone: false,
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+  public characters: Character[] = [
+    {
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 12000,
+    },
+    {
+      name: 'Vegeta',
+      power: 8000,
+    },
+  ];
+}
